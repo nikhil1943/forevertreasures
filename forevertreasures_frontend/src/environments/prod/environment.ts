e1435800f95a4c003789614edbf1ffd4ddeea137
nikhil1943 <nikhil.dulani1234@gmail.com>
2026-08-16 04:02:04 +0530

    Stop tracking environment configuration

--- forevertreasures_frontend/src/environments/prod/environment.ts
+++
@@ -1,7 +0,0 @@
-
-export const environment = {
-  production: true,
-  assetUrl: 'assets', // Base URL for your assets
-  apiUrl: 'http://localhost:8000/api', // Base URL for your backend
-  supportEmail: 'connect@forevertreasures.in' // Support email address
-};
