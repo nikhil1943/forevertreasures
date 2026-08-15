0a5ae43846072efc61c52db68e3366552d350095
nikhil1943 <nikhil.dulani1234@gmail.com>
2026-08-16 05:06:08 +0530

    Updated gitignore to include environment file in angular frontend for deployment on vercel.

---
+++ forevertreasures_frontend/src/environments/prod/environment.ts
@@ -0,0 +1,7 @@
+
+export const environment = {
+  production: true,
+  assetUrl: 'assets', // Base URL for your assets
+  apiUrl: 'https://forevertreasures-backend.onrender.com/api', // Base URL for your backend
+  supportEmail: 'connect@forevertreasures.in' // Support email address
+};
